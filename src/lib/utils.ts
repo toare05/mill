66768b6d4727c9e5730651416c735e94a79732ce
toare05 <toarete05@gmail.com>
2025-03-08 14:17:48 +0900

    folder

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-/**
- * Combines multiple class names into a single string, merging Tailwind CSS classes properly.
- * This is used by shadcn/ui components.
- */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 } 
